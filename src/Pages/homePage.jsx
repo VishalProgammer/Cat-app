e203550c4e8ddfd1e092385ebce94e4987232a64
Vishal <swamivishal14781516@gmail.com>
2024-10-19 01:25:29 +0530

    hehe

--- src/Pages/homePage.jsx
+++ src/Pages/homePage.jsx
@@ -1,8 +1,11 @@
+import './home.css'
+
 const Home = (props)=>{
     return(
         <div id="body">
-            <img src=".\Assests\cat-app-logo.png" alt="cat-logo" />
-            <h1>CatWiki</h1>
+            <img id="logo" src="src/Assests/cat-app-logo.png" alt="cat-logo" />
+            <h1 id='heading'>CatWiki</h1>
+            <p><q>Cats; They are much more than just Beautiful</q></p>
         </div>
     )
 }
