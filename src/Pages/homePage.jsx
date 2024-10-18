1f66b28c4ae1cbc95c0de027ea0508c3c59cb36c
Vishal <swamivishal14781516@gmail.com>
2024-10-19 01:16:28 +0530

    lol

--- src/Pages/homePage.jsx
+++ src/Pages/homePage.jsx
@@ -1,7 +1,7 @@
 const Home = (props)=>{
     return(
         <div id="body">
-            <img src="https://i.pinimg.com/736x/dc/60/1f/dc601fef0e4e88cefd4da27c3fab6bc2.jpg" alt="cat-logo" />
+            <img src=".\Assests\cat-app-logo.png" alt="cat-logo" />
             <h1>CatWiki</h1>
         </div>
     )
