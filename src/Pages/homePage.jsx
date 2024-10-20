b2e5f2138920d0b21a0d1c4ecebe6feb208862d2
Vishal <swamivishal14781516@gmail.com>
2024-10-21 05:07:47 +0530

    Update homePage.jsx

--- src/Pages/homePage.jsx
+++ src/Pages/homePage.jsx
@@ -3,7 +3,7 @@ import './home.css'
 const Home = (props)=>{
     return(
         <div id="Homebody">
-            <img id="logo" src="src/Assests/cat-app-logo.png" alt="cat-logo" />
+            <img id="logo" src="https://i.imgur.com/XZv7T1H.png" alt="cat-logo" />
             <h1 id='heading'>CatWiki</h1>
             <p id='quote'><q>Cats; They are much more than just Beautiful</q></p>
         </div>
